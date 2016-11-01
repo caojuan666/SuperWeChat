97ddfc90b760b451ca5012faf509b94ff7beca88
caojuan <2922475655@qq.com>
2016-11-01 15:03:04 +0800

    添加Butterknife和并且跳转到登陆注册页面

--- ChatDemoUI3.0/src/cn/ucai/superwechat/ui/GuideActivity.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/ui/GuideActivity.java
@@ -1,8 +1,12 @@
 package cn.ucai.superwechat.ui;
 
 import android.os.Bundle;
+import android.view.View;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import cn.ucai.superwechat.R;
+import cn.ucai.superwechat.utils.MFGT;
 
 public class GuideActivity extends BaseActivity {
 
@@ -10,6 +14,18 @@ public class GuideActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guide);
+        ButterKnife.bind(this);
     }
 
+    @OnClick({R.id.btt_register_login, R.id.btt_register_signup})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btt_register_login:
+                MFGT.gotoLogin(this);
+                break;
+            case R.id.btt_register_signup:
+                MFGT.gotoRegister(this);
+                break;
+        }
+    }
 }
