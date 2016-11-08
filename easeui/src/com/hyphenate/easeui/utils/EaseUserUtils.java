485f37b1decc2d4b82699c0b4dfcb6b8f4688448
caojuan <2922475655@qq.com>
2016-11-08 14:48:09 +0800

    及时更新新朋友的最新头像

--- easeui/src/com/hyphenate/easeui/utils/EaseUserUtils.java
+++ easeui/src/com/hyphenate/easeui/utils/EaseUserUtils.java
@@ -105,6 +105,27 @@ public class EaseUserUtils {
             Glide.with(context).load(R.drawable.default_avatar).into(imageView);
         }
     }
+    /**
+     * set user avatar
+     * @param
+     *
+     *      */
+//    及時更新頭像
+    public static void setAppPathUserAvatar(Context context, String path, ImageView imageView){
+
+        if(path==null){
+
+            try {
+                int avatarResId = Integer.parseInt(path);
+                Glide.with(context).load(avatarResId).into(imageView);
+            } catch (Exception e) {
+                //use default avatar
+                Glide.with(context).load(path).diskCacheStrategy(DiskCacheStrategy.ALL).placeholder(R.drawable.default_avatar).into(imageView);
+            }
+        }else{
+            Glide.with(context).load(R.drawable.default_avatar).into(imageView);
+        }
+    }
 
     /**
      * set user's nickname
